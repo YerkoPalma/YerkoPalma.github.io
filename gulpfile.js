0faddc12b31bed852287bbf10336a706b1914915
YerkoPalma <yerko.palma@usach.cl>
2015-11-30 23:50:38 -0300

    updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  less = require('gulp-less'),
+  browserify = require('gulp-browserify'),
+  plumber = require('gulp-plumber'),
+  fs = require('fs');
 
 gulp.task('webserver', function() {
   connect.server({
@@ -7,4 +11,20 @@ gulp.task('webserver', function() {
   });
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('less', function() {
+  if( fs.existsSync('assets/css/style.css')){
+    fs.unlink('assets/css/style.css');
+  }
+
+  gulp.src('assets/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('styles/*.less', ['less']);
+})
+
+gulp.task('default', ['less', 'webserver', 'watch']);
