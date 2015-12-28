31f7fa7c923b11a8458f7721805a2f1ca70192d1
YerkoPalma <yerko.palma@usach.cl>
2015-12-28 00:02:59 -0300

    preliminar projects design

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,11 +1,47 @@
 <template>
   <section id="projects" class="projects-section">
-      <div class="container">
-          <div class="row">
-              <div class="col-lg-12">
-                  <h1>Projects Section</h1>
+    <h1>¿Qué he hecho?</h1>
+      <div class="container-fluid">
+        <div class="projects-wrapper">
+          <div class="project-thumbnail" id="palmacontabilidad">
+            <div class="project-content">
+              <div class="project-hover">
+                <i class="fa fa-plus fa-5x"></i>
               </div>
+              <img class="img-responsive" src="assets/img/palmacontabilidad-thumbnail.png"/>
+            </div>
+            <div class="project-caption">
+              <h3>Palma Contabilidad</h3>
+              <span class="sub-title">Website design</span>
+            </div>
           </div>
+
+          <div class="project-thumbnail" id="salvador">
+            <div class="project-content">
+              <div class="project-hover">
+                <i class="fa fa-plus fa-5x"></i>
+              </div>
+              <img class="img-responsive" src="assets/img/salvador.png"/>
+            </div>
+            <div class="project-caption">
+              <h3>Salvador Aarón</h3>
+              <span class="sub-title">Website design</span>
+            </div>
+          </div>
+
+          <div class="project-thumbnail" id="chilena">
+            <div class="project-content">
+              <div class="project-hover">
+                <i class="fa fa-plus fa-5x"></i>
+              </div>
+              <img class="img-responsive" src="assets/img/chilena-thumbnail.png"/>
+            </div>
+            <div class="project-caption">
+              <h3>Chilena Reembolsos</h3>
+              <span class="sub-title">Mobile app</span>
+            </div>
+          </div>
+        </div>
       </div>
   </section>
 </template>
