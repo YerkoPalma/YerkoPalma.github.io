16c9f6f40323ad22aca52f5d415b1eb456133052
YerkoPalma <yerko.palma@usach.cl>
2016-01-10 20:23:10 -0300

    added projects data

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -3,48 +3,25 @@
     <h1>¿Qué he hecho?</h1>
       <div class="container-fluid">
         <div class="projects-wrapper">
-          <div class="project-thumbnail" id="palmacontabilidad">
-            <div class="project-content">
-              <div class="project-hover" v-on:click="fadeIn()">
-                <i class="fa fa-plus fa-5x"></i>
+          <template v-for="project in projects">
+            <div class="project-thumbnail" id="{{project.id}}">
+              <div class="project-content">
+                <div class="project-hover" v-on:click="fadeIn()">
+                  <i class="fa fa-plus fa-5x"></i>
+                </div>
+                <img class="img-responsive" src="{{project.thumbnail}}"/>
               </div>
-              <img class="img-responsive" src="assets/img/palmacontabilidad-thumbnail.png"/>
-            </div>
-            <div class="project-caption">
-              <h3>Palma Contabilidad</h3>
-              <span class="sub-title">Website design</span>
-            </div>
-          </div>
-
-          <div class="project-panel" id="palmacontabilidad-modal">
-            <i v-on:click="fadeOut()" class="fa fa-times fa-5x"></i>
-          </div>
-
-          <div class="project-thumbnail" id="salvador">
-            <div class="project-content">
-              <div class="project-hover">
-                <i class="fa fa-plus fa-5x"></i>
+              <div class="project-caption">
+                <h3>{{project.name}}</h3>
+                <span class="sub-title">{{project.description}}</span>
               </div>
-              <img class="img-responsive" src="assets/img/salvador.png"/>
             </div>
-            <div class="project-caption">
-              <h3>Salvador Aarón</h3>
-              <span class="sub-title">Website design</span>
-            </div>
-          </div>
 
-          <div class="project-thumbnail" id="chilena">
-            <div class="project-content">
-              <div class="project-hover">
-                <i class="fa fa-plus fa-5x"></i>
-              </div>
-              <img class="img-responsive" src="assets/img/chilena-thumbnail.png"/>
+            <div class="project-panel" id="{{project.id}}-modal">
+              <span v-on:click="fadeOut()" class="times">&times;</span>
             </div>
-            <div class="project-caption">
-              <h3>Chilena Reembolsos</h3>
-              <span class="sub-title">Mobile app</span>
-            </div>
-          </div>
+          </template>
+
         </div>
       </div>
   </section>
@@ -68,6 +45,19 @@ export default{
         el.classList.remove('shown');
       else
         el.className = el.className.replace(new RegExp('(^|\\b)' + 'shown'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  },
+  data() {
+    return {
+      projects: [
+        { id: 'palmacontabilidad', name: 'Palma Contabilidad', description: 'Website design', thumbnail: 'assets/img/palmacontabilidad-thumbnail.png', content: '' },
+        { id: 'salvador', name: 'Salvador Palma Navea', description: 'Website design', thumbnail: 'assets/img/salvador.png', content: '' },
+        { id: 'chilena', name: 'Reembolsos Movil', description: 'Mobile app', thumbnail: 'assets/img/chilena-thumbnail.png', content: '' },
+        { id: 'observatorioweb', name: 'Observatorio Web', description: 'Web App', thumbnail: '', content: '' },
+        { id: 'github', name: 'Github Projects', description: 'Open Source Projects', thumbnail: '', content: '' },
+        { id: 'codepen', name: 'CodePen Projects', description: 'Open Source Pens', thumbnail: '', content: ''}
+
+      ]
     }
   }
 }
