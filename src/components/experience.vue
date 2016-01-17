ce7ab22653db74f4f951cac8fbf7747cc937f5fb
YerkoPalma <yerko.palma@usach.cl>
2016-01-17 20:58:45 -0300

    first changes on experience section

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -2,8 +2,9 @@
   <section id="experience" class="experience-section">
       <div class="container">
           <div class="row">
+              <h1>Experience Section</h1>
               <div class="col-lg-12">
-                  <h1>Experience Section</h1>
+                  <div class="event"></div>
               </div>
           </div>
       </div>
