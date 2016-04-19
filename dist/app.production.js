d1b2ce9f1e2e1450c50547846b30a6bfe7001ec9
Yerko Palma <yerko.palma@usach.cl>
2016-04-19 18:57:14 +0000

    updated style of xperience section

--- dist/app.production.js
+++ dist/app.production.js
@@ -12399,18 +12399,33 @@ exports.default = {
     return {
       experience: [{
         title: 'Titulo Test',
+        year: '2012',
         image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDI0MiAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MjAwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTU0MmFkZjk1MWUgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMnB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNTQyYWRmOTUxZSI+PHJlY3Qgd2lkdGg9IjI0MiIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSI4OS44NTkzNzUiIHk9IjEwNS4xIj4yNDJ4MjAwPC90ZXh0PjwvZz48L2c+PC9zdmc+',
         content: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
       }, {
         title: 'Titulo Test',
+        year: '2013',
         image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjQyIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDI0MiAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MjAwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTU0MmFkZjk1MWUgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMnB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNTQyYWRmOTUxZSI+PHJlY3Qgd2lkdGg9IjI0MiIgaGVpZ2h0PSIyMDAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSI4OS44NTkzNzUiIHk9IjEwNS4xIj4yNDJ4MjAwPC90ZXh0PjwvZz48L2c+PC9zdmc+',
         content: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
       }]
     };
+  },
+  ready: function ready() {
+    var eventHeight = document.querySelector('.event-div').offsetHeight;
+    var nodes = document.querySelectorAll('.node');
+    var years = document.querySelectorAll('.node-year');
+
+    if (nodes) {
+      for (var i = 0; i < nodes.length; i++) {
+        nodes[i].style.top = eventHeight * i + 'px';
+        years[i].style.top = eventHeight * i + 'px';
+      }
+      document.querySelector('.timeline').style.height = eventHeight * nodes.length + 'px';
+    }
   }
 };
 if (module.exports.__esModule) module.exports = module.exports.default
-;(typeof module.exports === "function"? module.exports.options: module.exports).template = "\n  <section id=\"experience\" class=\"experience-section\">\n      <div class=\"container\">\n          <div class=\"row\">\n              <h1>Experiencia</h1>\n              <div class=\"timeline\">\n                <div class=\"node\"></div>\n                <div class=\"node\"></div>\n              </div>\n              <div v-for=\"event in experience\" class=\"row\">\n                <div class=\"col-sm-6 col-md-6\" :class=\"{ 'col-md-offset-6': $index % 2 !== 0 }\"> \n                  <div class=\"thumbnail thumbnail-event\" :class=\"{ 'event-left': $index % 2 === 0, 'event-right': $index % 2 !== 0 }\"> \n                    <img data-src=\"holder.js/100%x200\" alt=\"100%x200\" :src=\"event.image\" data-holder-rendered=\"true\" style=\"height: 200px; width: 100%; display: block;\">\n                    <div class=\"caption\"> \n                      <h3>{{event.title}}</h3> \n                      <p>{{event.content}}</p> \n                    </div> \n                  </div> \n                </div>\n              </div>\n          </div>\n      </div>\n  </section>\n"
+;(typeof module.exports === "function"? module.exports.options: module.exports).template = "\n  <section id=\"experience\" class=\"experience-section\">\n      <div class=\"container\">\n          <div class=\"row\">\n              <h1>Experiencia</h1>\n              <div class=\"timeline\">\n                <template v-for=\"event in experience\">\n                  <span class=\"node-year\">{{event.year}}</span>\n                  <div class=\"node\"></div>\n                </template>\n              </div>\n              <div v-for=\"event in experience\" class=\"row event-div\">\n                <div class=\"col-sm-5 col-md-5\" :class=\"{ 'col-md-offset-7': $index % 2 !== 0 }\"> \n                  <div class=\"thumbnail thumbnail-event\" :class=\"{ 'event-left': $index % 2 === 0, 'event-right': $index % 2 !== 0 }\"> \n                    <img data-src=\"holder.js/100%x200\" alt=\"100%x200\" :src=\"event.image\" data-holder-rendered=\"true\" style=\"height: 200px; width: 100%; display: block;\">\n                    <div class=\"caption\"> \n                      <h3>{{event.title}}</h3> \n                      <p>{{event.content}}</p> \n                    </div> \n                  </div> \n                </div>\n              </div>\n          </div>\n      </div>\n  </section>\n"
 if (module.hot) {(function () {  module.hot.accept()
   var hotAPI = require("vue-hot-reload-api")
   hotAPI.install(require("vue"), true)
